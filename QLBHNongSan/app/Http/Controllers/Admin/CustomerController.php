6c19b40300c07a49dd26262cd31dc9841caa8213
Mèo Méo Meo Mèo Meo <56384303+QuocThinh230600@users.noreply.github.com>
2020-06-20 00:08:25 +0700

    FIX
    fix

--- QLBHNongSan/app/Http/Controllers/Admin/CustomerController.php
+++ QLBHNongSan/app/Http/Controllers/Admin/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use DB, DATATIME;
+use DB, DataTime;
 
 class CustomerController extends Controller
 {
