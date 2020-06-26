18ce5efd91366146740d5b720962ed5fd8d050bf
Mèo Méo Meo Mèo Meo <56384303+QuocThinh230600@users.noreply.github.com>
2020-06-26 16:43:27 +0700

    add quảng cáo và khuyến mãi
    Đã xong

---
+++ QLBHNongSan/app/Http/Controllers/Admin/SaleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB, DateTime;
+
+class SaleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = DB::table('khuyenmai')->orderBy('id', 'DESC')->get();
+        return view('api-admin.modules.sale.index', ['khuyenmai' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $khuyenmai = DB::table('khuyenmai')->get();
+        return view('api-admin.modules.sale.create',['khuyenmai' => $khuyenmai]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->except('_token');
+        $data['created_at'] = new DateTime;
+        $data['updated_at'] = new DateTime;
+        //$request->anh->store('images', 'public');
+
+        DB::table('khuyenmai')->insert($data);
+
+        return redirect()->route('admin.sale.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $khuyenmai = DB::table('khuyenmai')->where('id',$id)->first();
+        return view('api-admin.modules.sale.edit', ['khuyenmai' => $khuyenmai]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->except('_token');
+        $data['updated_at'] = new DateTime;
+        DB::table('khuyenmai')->where('id',$id)->update($data);
+        return redirect()->route('admin.sale.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('khuyenmai')->where('id',$id)->delete();
+        return redirect()->route('admin.sale.index');
+    }
+}
